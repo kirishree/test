40c72365f42781ad4e2a4eb4ec7edfb313371d46
kirishree <115062611+kirishree@users.noreply.github.com>
2025-11-19 14:43:05 +0530

    Create s6.sh

---
+++ s6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+sudo apt -y install libpcre3-dev
+sudo apt-get -y install libevent-2.1-7
+sudo apt-get -y install libevent-pthreads-2.1-7
+sudo apt-get -y install libpthread-stubs0-dev
+sudo apt-get -y install libresolv-wrapper
+sudo apt-get -y install libiconv-hook1
+sudo apt -y install libz3-4
+sudo apt-get -y install libc-bin
+sudo apt-get -y install libpq5
+sudo apt -y install libopenipmi0
+sudo apt-get -y install libssh2-1
+sudo apt-get -y install libcurl4
+sudo apt -y install libxml2
+sudo apt -y install snmp
+apt install snmpd snmp libsnmp-dev -y
+sudo apt-get -y install unixodbc-common
+sudo apt -y install libgnutls28-dev
+sudo apt-get -y install libldap2-dev
+sudo apt -y install fping
+sudo apt-get install openipmi
+sudo apt-get install libopenipmi-dev
+sudo apt-get install libevent-dev
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libpcre3-dev
+sudo apt-get install libpq-dev
+sudo apt-get install libssh2-1-dev
+sudo apt-get install libevent-dev
+sudo apt -y install libpcre2-dev libevent-2.1-7 libevent-pthreads-2.1-7 libevent-pthreads-2.1-7 libpthread-stubs0-dev libresolv-wrapper libiconv-hook1 libz3-4
+sudo apt -y install libc-bin libpq5 libopenipmi0 libssh2-1 libcurl4  libxml2 snmpd snmp libsnmp-dev unixodbc-common libgnutls28-dev libldap2-dev fping
+sudo apt -y install openipmi libopenipmi-dev libcurl4-openssl-dev libpq-dev libssh2-1-dev libevent-dev
+sudo apt install -y clang gcc make cmake autoconf 
