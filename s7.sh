69f63afe9e6a2e7e2740033eda1bb2d8e2be5c88
kirishree <115062611+kirishree@users.noreply.github.com>
2025-11-19 14:54:39 +0530

    Create s7.sh

---
+++ s7.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://go.dev/dl/go1.24.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
