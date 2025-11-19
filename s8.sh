dbdcd32bb0fddf2a8c87d73f5f2fa78e2c5b1787
kirishree <115062611+kirishree@users.noreply.github.com>
2025-11-19 15:03:22 +0530

    Update s8.sh

--- s8.sh
+++ s8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://cdn.zabbix.com/zabbix/sources/stable/6.4/zabbix-6.4.20.tar.gz
-tar -zxvf zabbix-6.4.20.tar.gz
+wget https://cdn.zabbix.com/zabbix/sources/stable/7.0/zabbix-7.0.21.tar.gz
+tar -zxvf zabbix-7.0.21.tar.gz
 addgroup --system --quiet zabbix
 adduser --quiet --system --disabled-login --ingroup zabbix --home /var/lib/zabbix --no-create-home zabbix
