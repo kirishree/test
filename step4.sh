1510143f0cd169e0c878cba7b202799aa53ed091
kirishree <115062611+kirishree@users.noreply.github.com>
2025-11-19 13:04:16 +0530

    Create step4.sh

---
+++ step4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install apache2
+sudo apt install software-properties-common ca-certificates lsb-release apt-transport-https -y
+LC_ALL=C.UTF-8 sudo add-apt-repository ppa:ondrej/php 
+sudo apt update 
+sudo apt install php8.1 
+sudo apt install libapache2-mod-php
+sudo apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath php8.1-ldap php8.1-pgsql
