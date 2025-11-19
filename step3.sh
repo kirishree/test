d6ae7b62b7cb8e4c6c25565c3c92db9bd7e95873
kirishree <115062611+kirishree@users.noreply.github.com>
2025-11-19 12:57:14 +0530

    Create step3.sh

---
+++ step3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo ln -s /usr/local/pgsql/bin/createuser /usr/local/bin/createuser
+sudo ln -s /usr/local/pgsql/bin/createdb /usr/local/bin/createdb
+sudo ln -s /usr/local/pgsql/bin/psql /usr/local/bin/psql
+sudo ln -s /usr/local/pgsql/bin/ /usr/local/bin/
